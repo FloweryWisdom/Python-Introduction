9f86e89d4e34278ab49082b439509ee683e19df9
FloweryWisdom <srgerardoflorencio@gmail.com>
2024-05-17 01:39:05 -0700

    Pushing Assingment 8.

---
+++ Assignment-8/main.py
@@ -0,0 +1,14 @@
+def multiplication_table(number):
+    #iteratre from 1 to 12.
+    for i in range(1, 13):
+        #Calculate the product of the current number and loop index. 
+        result = number * i
+        #Print the result in the format "number x i = result"
+        print(f"{number} x {i} = {result}")
+
+#Get the number from the user. 
+number = int(input("Please enter a number to generate its multiplication table: "))
+
+
+#Call the function to gnerrate the multiplcation table. 
+multiplication_table(number)
