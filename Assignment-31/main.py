d107e091d8d1ef9a85247fcd83d9b0e24fa0f23d
FloweryWisdom <srgerardoflorencio@gmail.com>
2024-05-24 13:08:11 -0700

    Pushing unifinished changes in Assignment 31

---
+++ Assignment-31/main.py
@@ -0,0 +1,9 @@
+#Create a program the contents of the file 'text.txt' in the 'Assignment-29' folder.
+
+#Speicify the full file path
+filepath = "/Users/gerardoflorencioarcesanchez/Kodemia/Python-Introduction/Assignment-29/test.txt"
+
+#Open the file in read mode ("r") to read the contents
+with open(filepath, "r") as file:
+    #read the contents of the file
+     printf
