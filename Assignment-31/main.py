f91773ec41f10e9a16403d06c9187363cc2f609f
FloweryWisdom <srgerardoflorencio@gmail.com>
2024-05-24 15:14:23 -0600

    Pushing finished Assignment 31

--- Assignment-31/main.py
+++ Assignment-31/main.py
@@ -1,9 +1,13 @@
 #Create a program the contents of the file 'text.txt' in the 'Assignment-29' folder.
 
 #Speicify the full file path
-filepath = "/Users/gerardoflorencioarcesanchez/Kodemia/Python-Introduction/Assignment-29/test.txt"
+filepath = "c:/Users/srger/Kodemia/Python-Introduction/Assignment-29/test.txt"
 
 #Open the file in read mode ("r") to read the contents
 with open(filepath, "r") as file:
     #read the contents of the file
-     printf
+    content = file.read()
+
+    #Print the contents of the file
+    print(content)
+
