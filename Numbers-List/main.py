7b5923ae96b2e5e757c9f5bb0ee4eedf0ea9df49
FloweryWisdom <srgerardoflorencio@gmail.com>
2024-06-02 14:01:48 -0700

    Forgot to stage and commit a piece of code.

--- Numbers-List/main.py
+++ Numbers-List/main.py
@@ -1,5 +1,5 @@
 #Generate and print numbers from 0 to 255 with their hexadecimal representations
-def integer_to_binary (n):
+def integer_to_binary(n):
     if n == 0:
         return "00000000"
     binary_digits = []
@@ -8,3 +8,9 @@ def integer_to_binary (n):
         n = n // 2
     binary_str = ''.join(reversed(binary_digits))
     return binary_str.zfill(8) 
+
+def integer_to_hexadecimal(n):
+    if n == 0:
+        return "0x0"
+    hex_digits = []
+    
